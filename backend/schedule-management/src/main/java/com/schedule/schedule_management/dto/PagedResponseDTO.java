0e953c34e0641f43cc90badf12a81ffba0b44c17
abunahim <abunahim5@gmail.com>
2026-04-12 23:45:04 +0100

    Add pagination and filtering with PagedResponseDTO

---
+++ backend/schedule-management/src/main/java/com/schedule/schedule_management/dto/PagedResponseDTO.java
@@ -0,0 +1,19 @@
+package com.schedule.schedule_management.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PagedResponseDTO<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
